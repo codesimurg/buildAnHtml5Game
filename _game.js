24bb2fbd00711eece3e2c80776bc1377150f81d5
codesimurg <33839667+codesimurg@users.noreply.github.com>
2018-01-03 21:23:41 +0000

    Update _game.js

--- _game.js
+++ _game.js
@@ -1,1 +1,13 @@
-
+var PopBubbles = window.PopBubbles || {};
+PopBubbles.Game = (function($){
+	var Game = function(){
+		this.init = function(){
+			$(".but_start_game").bind("click",startGame);
+		};
+		var startGame = function(){
+			$(".but_start_game").unbind("click");
+			PopBubbles.ui.hideDialog();
+		};
+	};
+	return Game;
+})(jQuery);
